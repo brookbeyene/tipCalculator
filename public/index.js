56776455bd4bf0be06d6cd495a2a3743152588a8
keshawn0998 <keshawn@contenda.co>
2021-12-10 12:59:41 -0500

    Just some comments for presenting

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
+// A library formatter that allows us to convert every number into dollars 
 let formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',  
     });
+
+// Creates a new tip for the side menu we use this function alot within the app 
 let newTip = (tip) => {
     let name = document.createElement("h3")
     let u = document.createElement("u")
@@ -38,14 +41,12 @@ let newTip = (tip) => {
     itemDiv.appendChild(tipCash)
     itemDiv.appendChild(saveEdit)
     itemDiv.appendChild(deleteEdit)
-
-    
-    
-    // document.querySelector("#nav-items").appendChild(inputItems)
-
     itemDiv.appendChild(total)
     itemDiv.appendChild(document.createElement("hr"))
 }
+
+
+
 if (localStorage.getItem("SavedTips") && JSON.parse(localStorage.getItem("SavedTips")).length > 0){
     JSON.parse(localStorage.getItem("SavedTips")).forEach(tip => {
        newTip(tip)
@@ -54,7 +55,7 @@ if (localStorage.getItem("SavedTips") && JSON.parse(localStorage.getItem("SavedT
 let tipValues = [];
 
 
-
+// Calclates the whole tip from the cash side not the percent side 
 function cashTip(){
     let formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -85,16 +86,20 @@ function cashTip(){
         console.warn('added', {tipValues});
         localStorage.setItem("My personal pip", JSON.stringify(movies))
     }
-
+    console.log(formatter.format(Number(cashInput.value) + Number(cashTip.value)))
+    if (formatter.format(Number(cashInput.value) + Number(cashTip.value)) === "$NaN"){
+        alert("Please Enter a Value")
+    }
+    else{
     document.querySelector('.output').value = formatter.format(Number(cashInput.value) + Number(cashTip.value))
-
+    }
     document.getElementById
     // document.getElementById("TotalCashTipValue").innerHTML = formatter.format(Number(cashInput.value) + Number(cashTip.value))
 
     
 }
 
-
+// Calculates the percent side 
 function percetTip(){
     let formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -104,25 +109,35 @@ function percetTip(){
 let percentNumberInput = document.querySelector("#percentNumberInput")
 let totalPercentTip = document.querySelector("#totalPercentTip")
 let percentButton = document.querySelector("#percentButton")
-document.querySelector("#percentTipOutput").value = formatter.format(Number(percentBillInput.value) + (Number(percentBillInput.value) * Number(percentNumberInput.value)/100))
+if (formatter.format(Number(percentBillInput.value) + Number(percentNumberInput.value)) === "$NaN"){
+    alert("Please Enter a Value")
+}
+else{
+    document.querySelector("#percentTipOutput").value = formatter.format(Number(percentBillInput.value) + (Number(percentBillInput.value) * Number(percentNumberInput.value)/100))
+}
 }
 
+
+// This opens the Sidebar
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
   }
-  
+  // This closes the Sidebar
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-  let openSideNavButton = document.querySelector("#openSideNavButton")
-  openSideNavButton.addEventListener("click",(e) =>{
-    document.getElementById("mySidenav").style.width = "650px";
-  })
+
+
+// Saving the cash tip to the local stoage and side menu 
 let saveCashTip = (e) => {
     let cashNameInput = document.querySelector("#cashNameInput")
     let cashInput = document.querySelector("#cashInput")
     let cashTip = document.querySelector("#cashTip")
-    let objTip = {"id":JSON.parse(localStorage.getItem("SavedTips")).length + 1,"name":cashNameInput.value,"cashInput":cashInput.value,"cashTip":cashTip.value,"type":"cash","total":formatter.format(Number(cashTip.value) + Number(cashInput.value))}
+    if (formatter.format(Number(cashInput.value) + Number(cashTip.value)) === "$NaN"){
+        alert("Please Enter a Number")
+    }
+    else{
+    let objTip = {"id":!localStorage.getItem("SavedTips") ? 0 : JSON.parse(localStorage.getItem("SavedTips")).length + 1,"name":cashNameInput.value,"cashInput":cashInput.value,"cashTip":cashTip.value,"type":"cash","total":formatter.format(Number(cashTip.value) + Number(cashInput.value))}
     if (!localStorage.getItem("SavedTips") || JSON.parse(localStorage.getItem("SavedTips")).length === 0 ){
         localStorage.setItem("SavedTips",JSON.stringify([]))
     }
@@ -130,23 +145,35 @@ let saveCashTip = (e) => {
     newTip(objTip)
     console.log(cashNameInput.value,cashInput.value,cashTip.value)
 }
+}
+// Saving the percent side tip to the side menu 
 let savePercentTip = (e) => {
     let percentNameInput = document.querySelector("#percentNameInput")
     let percentBillInput = document.querySelector("#percentBillInput")
     let percentNumberInput = document.querySelector("#percentNumberInput")
-    let objTip = {"id":JSON.parse(localStorage.getItem("SavedTips")).length + 1,"name":percentNameInput.value,"cashInput":percentBillInput.value,"cashTip":percentNumberInput.value,"type":"percent","total":formatter.format(Number(percentBillInput.value) + (Number(percentBillInput.value) * Number(percentNumberInput.value)/100))}
+    if (formatter.format(Number(percentBillInput.value) + Number(percentNumberInput.value)) === "$NaN"){
+        alert("Please Enter A Number")
+    }
+    else {
+    let objTip = {"id":!localStorage.getItem("SavedTips") ? 0 : JSON.parse(localStorage.getItem("SavedTips")).length + 1,"name":percentNameInput.value,"cashInput":percentBillInput.value,"cashTip":percentNumberInput.value,"type":"percent","total":formatter.format(Number(percentBillInput.value) + (Number(percentBillInput.value) * Number(percentNumberInput.value)/100))}
     if (!localStorage.getItem("SavedTips") || JSON.parse(localStorage.getItem("SavedTips")).length === 0 ){ 
         localStorage.setItem("SavedTips",JSON.stringify([]))
     }
     localStorage.setItem("SavedTips",JSON.stringify([...JSON.parse(localStorage.getItem("SavedTips")),objTip]))
     newTip(objTip)
 }
-
+}
+// This is for editing the saved tips 
+// I used a type property so I can use the same function for both types of saved tips 
+// I wished I wouldve refactored more code 
 let saveEdit = tip => {
     console.log(tip)
     let element = document.querySelector(`[data-id="${tip.id}"]`)
     console.log(element.childNodes)
-
+    if (formatter.format(Number(element.childNodes[1].value) + Number(element.childNodes[3].value)) === "$NaN"){
+        alert("Please Enter A Number")
+    }
+    else {
     element.childNodes[6].textContent = tip.type === "cash" ? formatter.format(Number(element.childNodes[1].value) + Number(element.childNodes[3].value)) : formatter.format(Number(element.childNodes[1].value) + (Number(element.childNodes[1].value) * Number(element.childNodes[3].value)/100))
     let data = JSON.parse(localStorage.getItem("SavedTips")).map(item => {
         if (Number(item.id) === Number(tip.id)){
@@ -162,6 +189,9 @@ let saveEdit = tip => {
     
     localStorage.setItem("SavedTips",JSON.stringify(data))
 }
+}
+
+// The type property helped me save time and code
 let deleteTip = tip => {
     console.log(tip)
     let element = document.querySelector(`[data-id="${tip.id}"]`)
